dea17e068d681b158109dbb9cde925ebee3048ca
DemonicDemiser <DemonicDemiser@users.noreply.github.com>
2020-11-19 16:00:26 +0600

    update 1.59

--- registration.php
+++ registration.php
@@ -2,17 +2,28 @@
 
 session_start();
 
-
 $con = mysqli_connect('localhost', 'root', '' );
 
 mysqli_select_db($con, 'userregistration');
 
 $name = $_POST['name'];
-$pass = $_POST['surname'];
-$pass = $_POST['phoneNumber'];
-$pass = $_POST['login'];
-$pass = $_POST['email'];
-$pass = $_POST['password'];
+$surname = $_POST['surname'];
+$phoneNumber = $_POST['phoneNumber'];
+$login = $_POST['login'];
+$email = $_POST['email'];
+$password = $_POST['password'];
 
-$s = "select * from "
+$s = "select * from usertable where login - '$login'";
+
+$result = myslqi_query($con, $s);
+
+$num = mysqli_num_rows($result);
+
+if($num == 1){
+    echo "Username Already Taken";
+}else{
+    $reg = "insert into usertable(name, surname, phoneNumber, login, password) values ('$name','$surname','$phoneNumber','$login','$password')";
+    mysqli_query($con, $reg);
+    echo"Registration successfull";
+}
 ?>
