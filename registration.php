80075e0c9034189c8b6dc7d0eb5075db162d2512
DemonicDemiser <DemonicDemiser@users.noreply.github.com>
2020-11-19 15:29:29 +0600

    update 1.58

--- registration.php
+++ registration.php
@@ -7,5 +7,12 @@ $con = mysqli_connect('localhost', 'root', '' );
 
 mysqli_select_db($con, 'userregistration');
 
-$name = $_POST
+$name = $_POST['name'];
+$pass = $_POST['surname'];
+$pass = $_POST['phoneNumber'];
+$pass = $_POST['login'];
+$pass = $_POST['email'];
+$pass = $_POST['password'];
+
+$s = "select * from "
 ?>
